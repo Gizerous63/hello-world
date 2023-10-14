4b4a8571f0c97bf72c289f40b04eaa6e3940b9bc
Сергей <gizerous63@gmail.com>
2023-10-14 18:26:35 +0300

    Создан класс HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Mother"); //слова в двойных кавычках
+        System.out.print("Mother"); //  приставка ln делает переход на следующую строку
+        System.out.println("Афther");
+
+        System.out.print("Афther\n\n\n");// эскейп символ \n для прехода на новую строчку
+        System.out.print("Афther");
+
+        System.out.print("Афther\t\t\t"); // символ \t для табуляции слудующего слова в той же строке
+        System.out.println("Афther");
+
+        System.out.print("\t\t\tАфther\n");
+        System.out.print("bdsther");
+    }
+}

5209f207459d5436ed323709b4fcfa1990d67ec8
Сергей <gizerous63@gmail.com>
2023-10-14 19:48:01 +0300

    Создан класс Pravila

---
+++ src/Pravila.java
@@ -0,0 +1,11 @@
+public class Pravila {
+    public static void main(String[] args) { //количество открытых фигур скобок должено совпадать с закрытыми
+        System.out.println("Vfvf");
+        System.out.println("BGHHY");
+        System.out.println("BGTFDE"); // отступы должны быть ровными
+        System.out.println("BGTFDE аиыявм ям мыфавымиыя ыпваиыва пыппысмыкцф BGTFDE"
+                + " аиыявм ям мыфавымиыя ыпваиыва пыппысмыкцф BGTFDE аиыявм ям "
+                + "мыфавымиыя ыпваиыва пыппысмыкцф BGTFDE аиыявм ям "
+                + "мыфавымиыя ыпваиыва пыппысмыкцф"); // если большое преложение то наводим где угодно и через ENTER
+    }
+}

931e71a0f8ab91f55b08dd34ebc6cdffb9f6015e
Сергей <gizerous63@gmail.com>
2023-10-14 19:53:00 +0300

    Создан класс WhileLoops

---
+++ src/WhileLoops.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+//        boolean t = 5<10;
+//        System.out.println(t);
+//        boolean f = 11<10;
+//        System.out.println(f);
+//        boolean a = 5<=5; // может быть двоное равенство
+//        System.out.println(a);
+//        boolean b = 5==5; // ставится двоное равенство потомучто просто равество уже делает присвоение
+//        System.out.println(b);
+
+
+
+//        int value = 3;
+//        while (value<5){ // это условие цикла, тоесть до тех пок пока.....
+//            System.out.println("Hello");
+//            value = value+1; // для цикла задаем условия переменной, что с каждым циклом она на один больше
+//
+//
+//        }
+
+        System.out.println("*****Добро пожаловать в сравнитель");
+        System.out.println("Ведите превое число a");
+        int a = new Scanner(System.in).nextInt();
+        System.out.println("Ведите второе число b");
+        int b = new Scanner(System.in).nextInt();
+        boolean t = a<b;
+        System.out.println(t);
+
+//        System.out.println("******Добро пожаловать в цикл******");
+//        System.out.println("Введите превое число a");
+//        int a = new Scanner(System.in).nextInt();
+//        System.out.println("Введите превое число b");
+//        int b = new Scanner(System.in).nextInt();
+//        while (a<=b){
+//            System.out.println("Hello");
+//            a = a+1;
+//        }
+    }
+}

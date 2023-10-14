9d5cb8d5921f3dce1521ff11d48f848d005f0bce
Сергей <gizerous63@gmail.com>
2023-10-14 19:11:15 +0300

    Создан класс Stroki

---
+++ src/Stroki.java
@@ -0,0 +1,11 @@
+public class Stroki {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "Hello"; // Стинг тоже класс поэтому с большой буквы
+        String space = "+";
+        String name = "Buy";
+        System.out.println(s+space+name);
+        System.out.println("Hello"+ "+"+ "Buy");
+        System.out.println("My number is "+ x);
+    }
+}

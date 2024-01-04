6705928913b04191b7a95b90b573d5cea8f65e0b
Сергей <gizerous63@gmail.com>
2024-01-04 15:16:05 +0200

    Добавлен класс MetodiDva

--- src/MetodiDva.java
+++ src/MetodiDva.java
@@ -1,8 +1,44 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class MetodiDva {
-    public static void main(String[] args) {
+    static int number = 0;
+        public static void main(String[] args) {
+
+            output();
+
+//            while (true){
+//
+//                if (number % 2 == 0){
+//                    System.out.println("TRUE");
+//                }
+//                else {
+//                    System.out.println("FALSE");
+//                }
+//            }
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+
+    }
+
+    private static int inputNumber() {
+        System.out.println("Введите целое число:");
+        return new Scanner(System.in).nextInt();
+    }
+    private static void output() {
+
+//        while (true) {
+//            if (inputNumber() % 2 == 0) {
+//                System.out.println("TRUE");
+//            } else {
+//                System.out.println("FALSE");
+//            }
+//            System.out.println(inputNumber() % 2 == 0 ? true : false); можно через тернарник
+            System.out.println(inputNumber() % 2 == 0);// верхнюю запись проще можно так записать, но небудет повторятся цикл
+
+
+
     }
 }
